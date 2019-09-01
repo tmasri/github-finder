820bc1cddff5d0dcbc8389918fd89133f7ef3532
Tareq <tmasri58@gmail.com>
2019-08-31 22:28:46 -0400

    initial build with grabbing users on app mount(when the app starts running, and with a search bar to search for specific users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import NavBar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import Search from './components/users/Search'
+import axios from 'axios'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   state = {
+      users: [],
+      loading: false,
+   }
+
+   async componentDidMount() {
+      //  this is a life cycle method
+      //  this will run when the component runs
+      this.setState({ loading: true })
+      const res = await axios.get(
+         `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      )
+      this.setState({
+         users: res.data,
+         loading: false,
+      })
+   }
+
+   // Search Github users
+   searchUsers = async text => {
+      // this.setState({ loading: true })
+      const res = await axios.get(
+         `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      )
+
+      this.setState({
+         users: res.data.items,
+         loading: false,
+      })
+      console.log(text + ' from App.js')
+   }
+
+   render() {
+      return (
+         <div className='App'>
+            <NavBar />
+            <div className='container'>
+               <Search searchUsers={this.searchUsers} />
+               <Users loading={this.state.loading} users={this.state.users} />
+            </div>
+         </div>
+      )
+   }
 }
 
-export default App;
+export default App
