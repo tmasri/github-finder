2eb922098a3aee8261e4a818f599d4a4b782698b
Tareq <tmasri58@gmail.com>
2019-09-05 17:14:34 -0400

    added hooks to App.js and User.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,60 +1,55 @@
 // rce
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export class Search extends Component {
-   state = {
-      text: '',
-   }
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+   const [text, setText] = useState('');
 
-   static propTypes = {
-      searchUsers: PropTypes.func.isRequired,
-      clearUsers: PropTypes.func.isRequired,
-      showClear: PropTypes.bool.isRequired,
-      setAlert: PropTypes.func.isRequired
-   }
-
-   onSubmit = e => {
-      e.preventDefault()
-      if (this.state.text === '') {
-         this.props.setAlert('Please enter something', 'light')
+   const onSubmit = e => {
+      e.preventDefault();
+      if (text === '') {
+         setAlert('Please enter something', 'light');
       } else {
-         this.props.searchUsers(this.state.text)
-         this.setState({ text: '' })
+         searchUsers(text);
+         setText('');
       }
-   }
+   };
 
-   onChange = e => this.setState({ [e.target.name]: e.target.value })
+   const onChange = e => setText(e.target.value);
 
-   render() {
-      const { showClear, clearUsers } = this.props
-      return (
-         <div>
-            <form onSubmit={this.onSubmit} className='form'>
-               <input
-                  type='text'
-                  name='text'
-                  placeholder='Search Users...'
-                  value={this.state.text}
-                  onChange={this.onChange}
-               />
-               <input
-                  type='submit'
-                  value='Search'
-                  className='btn btn-dark btn-block'
-               />
-            </form>
-            {
-               // this.props.showClear && htmlContent -> this is like an if statement saying that is the first condition is true, show the second condition or run the second condition
-            }
-            {showClear && (
-               <button className='btn btn-light btn-block' onClick={clearUsers}>
-                  Clear
-               </button>
-            )}
-         </div>
-      )
-   }
-}
+   return (
+      <div>
+         <form onSubmit={onSubmit} className='form'>
+            <input
+               type='text'
+               name='text'
+               placeholder='Search Users...'
+               value={text}
+               onChange={onChange}
+            />
+            <input
+               type='submit'
+               value='Search'
+               className='btn btn-dark btn-block'
+            />
+         </form>
+         {
+            // showClear && htmlContent -> this is like an if statement saying that is the first condition is true, show the second condition or run the second condition
+         }
+         {showClear && (
+            <button className='btn btn-light btn-block' onClick={clearUsers}>
+               Clear
+            </button>
+         )}
+      </div>
+   );
+};
 
-export default Search
+Search.propTypes = {
+   searchUsers: PropTypes.func.isRequired,
+   clearUsers: PropTypes.func.isRequired,
+   showClear: PropTypes.bool.isRequired,
+   setAlert: PropTypes.func.isRequired,
+};
+
+export default Search;
