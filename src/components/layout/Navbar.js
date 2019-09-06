ac0fddcd2863fc7457843020cac563a8ca93797a
Tareq <tmasri58@gmail.com>
2019-09-06 10:13:11 -0400

    changed links in navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
    return (
@@ -17,17 +17,17 @@ const Navbar = ({ icon, title }) => {
             </li>
          </ul>
       </nav>
-   )
-}
+   );
+};
 
 Navbar.defaultProps = {
    title: 'Github Finder',
    icon: 'fab-fa-github',
-}
+};
 
 Navbar.propTypes = {
    title: PropTypes.string.isRequired,
    icon: PropTypes.string.isRequired,
-}
+};
 
-export default Navbar
+export default Navbar;
